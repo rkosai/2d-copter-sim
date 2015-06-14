2977586faeb1185789246bae8410ed4b9157dc40
Ryan Kosai <rk@ryankosai.com>
2015-06-13 19:54:07 -0700

    Refactor to move control code into it's own module.

--- js/KeyControls.js
+++ js/KeyControls.js
@@ -1,11 +1,9 @@
 function KeyControls() {
-    this.env = new Environment();
-
     this.q = false; // 81
     this.e = false; // 69
     this.w = false; // 87
 
-    this.session = null;
+    this.autopilot = new PositionControl();
 
     $(window).keydown(function (event) {
         if (event.which === 81) {
@@ -15,6 +13,7 @@ function KeyControls() {
             this.e = true;
         }
         else if (event.which === 87) {
+            if (!this.w) { this.lockLatch = true; }
             this.w = true;
         }
 
@@ -29,15 +28,19 @@ function KeyControls() {
         }
         else if (event.which === 87) {
             this.w = false;
-            this.session = null;
         }
 
     }.bind(this));
 }
 
 KeyControls.prototype.getThrust = function(sensors) {
+    if (this.lockLatch) {
+        this.autopilot.lockPosition(sensors);
+        this.lockLatch = false;
+    }
+
     if (this.w) {
-        return this._stabilize(sensors);
+        return this.autopilot.getThrust(sensors);
     }
     else {
         return ({
@@ -45,73 +48,4 @@ KeyControls.prototype.getThrust = function(sensors) {
             right: this.e ? 20 : 0
         });
     }
-};
-
-KeyControls.prototype._targetDelta = function(sensors, target) {
-    var dx = sensors.position.x +
-             sensors.velocity.x * this.env.step * 50 -
-             target.x;
-
-    var dy = sensors.position.y +
-             sensors.velocity.y * this.env.step * 30 -
-             target.y;
-
-    return { dx: dx, dy: dy };
-};
-
-KeyControls.prototype._stabilize = function(sensors) {
-    var left = 0, right = 0;
-
-    // Cache starting location
-    if (!this.session) {
-        this.session = {
-            x: sensors.position.x,
-            y: sensors.position.y
-        };
-    }
-
-    // Special case for out-of-control
-    if (sensors.velocity.theta > Math.PI) {
-        return { left: 20, right: 0 };
-    }
-    else if (sensors.velocity.theta > Math.PI) {
-        return { left: 0, right: 20 };
-    }
-
-    // Special case for upside down
-    //if (sensors.position.theta
-
-    // Calculating error
-    var t = this._targetDelta(sensors, this.session);
-
-    // Drive future angle to zero
-    var recovery_angle = t.dx * 0.1;
-    recovery_angle = Math.min(recovery_angle, Math.PI / 4);
-    recovery_angle = Math.max(recovery_angle, -1 * Math.PI / 4);
-
-    var s = sensors.position.theta +
-            sensors.velocity.theta * this.env.step * 15 -
-            recovery_angle;
-
-    if (s > 0.05) {
-        left = Math.min(s * 150, 4);
-        right = 0;
-    }
-    else if (s < -0.05) {
-        right = Math.min(Math.abs(s * 150), 4);
-        left = 0;
-    }
-
-    // If we're below the target height, and we're pointed in the right
-    // direction, run both engines.
-
-    if ( (t.dy < 0) && (Math.abs(s) < (Math.PI / 4)) ) {
-        left += Math.min(Math.abs(t.dy) * 10, 15);
-        right += Math.min(Math.abs(t.dy) * 10, 15);
-    }
-
-    return {
-        left: Math.min(left, 20),
-        right: Math.min(right, 20)
-    };
 };
---
+++ js/PositionControl.js
@@ -0,0 +1,73 @@
+function PositionControl() {
+    this.env = new Environment();
+    this.lock = { x: 0, y: 0 };
+}
+
+PositionControl.prototype.lockPosition = function(sensors) {
+    this.lock.x = sensors.position.x;
+    this.lock.y = sensors.position.y;
+};
+
+PositionControl.prototype.getThrust = function(sensors) {
+    var left = 0, right = 0;
+
+    var future_angle = sensors.position.theta +
+        sensors.velocity.theta * this.env.step * 15;
+
+
+    // Special case for out-of-control
+    if (sensors.velocity.theta > Math.PI / 2) {
+        return { left: 20, right: 0 };
+    }
+    else if (sensors.velocity.theta < -1 * Math.PI / 2) {
+        return { left: 0, right: 20 };
+    }
+
+    // TBD: Special case for low velocity and upside down
+
+    // Calculating error
+    var t = this._targetDelta(sensors, this.lock);
+
+    // Drive future angle to zero
+    var recovery_angle = t.dx * 0.1;
+    recovery_angle = Math.min(recovery_angle, Math.PI / 4);
+    recovery_angle = Math.max(recovery_angle, -1 * Math.PI / 4);
+
+    var s = future_angle - recovery_angle;
+
+    if (s > 0.05) {
+        left = Math.min(s * 150, 4);
+        right = 0;
+    }
+    else if (s < -0.05) {
+        right = Math.min(Math.abs(s * 150), 4);
+        left = 0;
+    }
+
+    // If we're below the target height, and we're pointed in the right
+    // direction, run both engines.
+
+    if ( (t.dy < 0) && (Math.abs(s) < (Math.PI / 4)) ) {
+        left += Math.min(Math.abs(t.dy) * 10, 15);
+        right += Math.min(Math.abs(t.dy) * 10, 15);
+    }
+
+    return {
+        left: Math.min(left, 20),
+        right: Math.min(right, 20)
+    };
+
+};
+
+PositionControl.prototype._targetDelta = function(sensors, target) {
+    var dx = sensors.position.x +
+             sensors.velocity.x * this.env.step * 50 -
+             target.x;
+
+    var dy = sensors.position.y +
+             sensors.velocity.y * this.env.step * 30 -
+             target.y;
+
+    return { dx: dx, dy: dy };
+};
+
